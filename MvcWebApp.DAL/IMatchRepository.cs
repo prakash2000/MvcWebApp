124cb7e80a183219c49fdb696e0b36e3f501ae16
prakash2000 <prakash1391@gmail.com>
2015-11-21 18:24:59 +0530

    v2
    v2

---
+++ MvcWebApp.DAL/IMatchRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvcWebApp.DomainModel;
+
+namespace MvcWebApp.DAL
+{
+    public interface IMatchRepository : IDisposable
+    {
+        IEnumerable<Match> GetMatches();
+        Match GetMatchByID(int matchID);
+        void InsertMatch(Match match);
+        void DeleteMatch(int matchID);
+        void UpdateMatch(Match match);
+        void Save();
+    }
+}
---
+++ MvcWebApp.DAL/MatchRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using MvcWebApp.DomainModel;
+
+namespace MvcWebApp.DAL
+{
+    public class MatchRepository:IMatchRepository,IDisposable
+    {
+        private DataContext context;
+
+        public MatchRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Match> GetMatches()
+        {
+            return context.Matches.ToList();
+        }
+
+        public Match GetMatchByID(int id)
+        {
+            return context.Matches.Find(id);
+        }
+
+        public void InsertMatch(Match newmatch)
+        {
+            context.Matches.Add(newmatch);
+        }
+
+        public void DeleteMatch(int matchID)
+        {
+            Match match = context.Matches.Find(matchID);
+            context.Matches.Remove(match);
+        }
+
+        public void UpdateMatch(Match match)
+        {
+            context.Entry(match).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
